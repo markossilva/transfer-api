05bfae67c405c822841217083268cb9b3cbc0557
Markos da Silva <markosrsilva@gmail.com>
2023-01-10 21:06:56 -0300

    feature: update client controller

--- src/main/java/br/com/itau/transferapi/application/rest/ClientController.java
+++ src/main/java/br/com/itau/transferapi/application/rest/ClientController.java
@@ -1,8 +1,11 @@
 package br.com.itau.transferapi.application.rest;
 
+import br.com.itau.transferapi.domain.exception.ApplicationException;
 import br.com.itau.transferapi.domain.model.*;
 import br.com.itau.transferapi.domain.service.ClientService;
 import br.com.itau.transferapi.domain.service.TransactionService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,15 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+  private static final Logger logger = LogManager.getLogger(ClientController.class);
+
+  private static final String CREATE = "/create";
+  private static final String ALL_CLIENTS = "/all";
+  private static final String TRANSFER_VALUE = "/transfer/amount";
+  private static final String WALLETS_BY_CLIENT = "/{clientId}/wallets";
+  private static final String BY_WALLET_AND_CLIENT_ID = "/{clientId}/wallet/{walletId}";
+  private static final String CLIENT_BY_WALLET_ID = "/wallet/{walletId}";
+  private static final String CLIENT_WALLET_TRANSACTIONS = "/wallet/transactions/{walletId}";
   private final ClientService service;
   private final TransactionService transactionService;
 
@@ -25,7 +37,11 @@ public class ClientController {
     this.transactionService = transactionService;
   }
 
-  @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(
+      path = CREATE,
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE
+  )
   ResponseEntity<RegisteredClient> createClient(@RequestBody final Object createOrderRequest) {
     final RegisteredClient clientTest = service
         .createNewClient(Client.builder()
@@ -36,12 +52,12 @@ public class ClientController {
     return ResponseEntity.ok(clientTest);
   }
 
-  @GetMapping
+  @GetMapping(path = ALL_CLIENTS)
   ResponseEntity<List<Client>> findAllClients() {
     return ResponseEntity.ok(service.findAllClients());
   }
 
-  @PutMapping
+  @PutMapping(path = TRANSFER_VALUE)
   ResponseEntity<BigInteger> doTransaction(@RequestParam final String originClientId,
                                            @RequestParam final String originWalletId,
                                            @RequestParam final String targetClientId,
@@ -60,18 +76,40 @@ public class ClientController {
     return ResponseEntity.ok(buildTransaction.getId());
   }
 
-  @GetMapping("/wallets")
+  @GetMapping(path = WALLETS_BY_CLIENT)
   ResponseEntity<List<Wallet>> findAllWalletsByClient(@RequestParam final String clientId) {
     return ResponseEntity.ok(service.findAllWallets(UUID.fromString(clientId)));
   }
 
-  @GetMapping("/wallet")
+  @GetMapping(path = BY_WALLET_AND_CLIENT_ID)
   ResponseEntity<Wallet> findWalletsById(@RequestParam final String clientId, @RequestParam final String walletId) {
-    return ResponseEntity.ok(service.findAWallet(UUID.fromString(clientId), UUID.fromString(walletId)));
+    try {
+      return ResponseEntity.ok(service
+          .findAWallet(UUID.fromString(clientId), UUID.fromString(walletId)));
+    } catch (Exception e) {
+      logger.error("Error to findWalletsById: wallet#{} - {}. Message: {}", clientId, walletId, e.getMessage());
+      throw new ApplicationException(e);
+    }
   }
 
-  @GetMapping("/wallet/id")
+  @GetMapping(path = CLIENT_BY_WALLET_ID)
   ResponseEntity<Client> findClientByWalletId(@RequestParam final String walletId) {
-    return ResponseEntity.ok(service.findClientByWalletId(UUID.fromString(walletId)));
+    try {
+      return ResponseEntity.ok(service.findClientByWalletId(UUID.fromString(walletId)));
+    } catch (Exception e) {
+      logger.error("Error to findClientByWalletId: wallet#{} - {}", walletId, e.getMessage());
+      throw new ApplicationException(e);
+    }
+  }
+
+  @GetMapping(path = CLIENT_WALLET_TRANSACTIONS)
+  ResponseEntity<List<WalletTransactions>> findAllByWalletId(@PathVariable final String walletId) {
+    try {
+      return ResponseEntity.ok(transactionService
+          .findAllByWalletId(UUID.fromString(walletId)));
+    } catch (Exception e) {
+      logger.error("Error to findAllByWalletId: wallet#{} - {}", walletId, e.getMessage());
+      throw new ApplicationException(e);
+    }
   }
 }
